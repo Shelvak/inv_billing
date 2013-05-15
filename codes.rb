dd296911d5bec45f7584ce0725472a583bb0a102
Néstor Coppi <nestorcoppi@gmail.com>
2013-05-14 22:17:18 -0300

    Comenzando facturación sacada del I.N.V.

---
+++ codes.rb
@@ -0,0 +1,34 @@
+# encoding: UTF-8
+
+CODIGOS = {
+  '103' => {
+    desc: 'mv-05 Compra venta',
+    precio1: 25,
+    precio2: 25
+  },
+  '106' => {
+    desc: 'mv-05 Corte otros vinos',
+    precio1: 25,
+    precio2: 25
+  },
+  '108' => {
+    desc: 'mv-05 Certificación',
+    precio1: 15,
+    precio2: 75
+  },
+  '115' => {
+    desc: 'mv-05 Elaboración',
+    precio1: 25,
+    precio2: 25
+  },
+  '127' => {
+    desc: 'mv-05 Cambio de propiedad',
+    precio1: 25,
+    precio2: 25
+  },
+  '231' => {
+    desc: 'mv-05 Aptitud de Exportación',
+    precio1: 25,
+    precio2: 25
+  }
+}
